f4f25ac69c7976af13a9667295c091d543467ef7
Joseph Broome <josephlloydbroome@gmail.com>
2022-08-02 11:16:27 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using KanyeWest;
+using Newtonsoft.Json.Linq;
+
+
+
+var client = new HttpClient();
+var quote = new QuoteGenerator(client);
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine("********************************");
+    Console.WriteLine($"Kanye: {quote.Kanye()}");
+
+    Console.WriteLine($"Ron Swanson: {quote.RonSwanson()}");
+    Console.WriteLine();
+
+}
+
+
+
---
+++ QuoteGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWest
+{
+    public class QuoteGenerator
+    {
+        private HttpClient _client;
+
+        public QuoteGenerator(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public string Kanye()
+        {
+            
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+
+           
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public string RonSwanson()
+        {
+           
+            var ronswansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronswansonResponse = _client.GetStringAsync(ronswansonURL).Result;
+            var ronswansonQuote = JArray.Parse(ronswansonResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim(); ;
+
+            return ronswansonQuote.ToString();
+        }
+    }
+}
